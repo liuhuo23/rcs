6b60d5fb2e509732ed923a3954eb6b31fe14436a
zhaoyajie <zhaoyj2370@163.com>
2023-04-23 00:18:52 +0800

    完成基本框架搭建

---
+++ request_test.go
@@ -0,0 +1,27 @@
+package rcs
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestAdd(t *testing.T) {
+	request := Request{
+		Method:   CREATE,
+		Send:     "echo hello",
+		Time:     time.Now(),
+		Sign:     "sdfsdfs",
+		UserName: "sdfsdf",
+		PassWord: "sdfsdf",
+		Content:  []byte("sdfsdfsdfs"),
+	}
+	fmt.Println(request.ToByte())
+}
+
+func TestInt(t *testing.T) {
+	str := "31"
+	length, _ := strconv.ParseInt(str, 10, 64)
+	fmt.Println(length)
+}
